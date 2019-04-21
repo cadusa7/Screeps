305cdf42edb05e87d768e3e3a634340467f01d6c
Timothy Gonyea <cadusaseven@gmail.com>
2019-04-20 20:21:44 -0400

    Updated main to include factory

--- main.js
+++ main.js
@@ -1,34 +1,23 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var factory = require('factory');
+
 
 module.exports.loop = function () {
 
-	for(var name in Memory.creeps) {
+   for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
     }
 
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
+    factory.run();
 
-    if(harvesters.length < 2) {
-        var newName = 'Harvester' + Game.time;
-        console.log('Spawning new harvester: ' + newName);
-        Game.spawns['CadusaBase'].spawnCreep([WORK,CARRY,MOVE], newName, 
-            {memory: {role: 'harvester'}});
-    }
-    
-    if(Game.spawns['CadusaBase'].spawning) { 
-        var spawningCreep = Game.creeps[Game.spawns['CadusaBase'].spawning.name];
-        Game.spawns['CadusaBase'].room.visual.text(
-            '🛠️' + spawningCreep.memory.role,
-            Game.spawns['CadusaBase'].pos.x + 1, 
-            Game.spawns['CadusaBase'].pos.y, 
-            {align: 'left', opacity: 0.8});
-    }
+
+
+   
 
     var tower = Game.getObjectById('5cb8b90b9ed8c5a047e16a31');
     if(tower) {
